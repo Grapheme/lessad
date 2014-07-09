34468140702c522150f0fa5892a3df5721280fba
KostyaD <thedamaxstudio@gmail.com>
2014-07-09 19:22:50 +0400

    pages

--- static/js/main.js
+++ static/js/main.js
@@ -11,8 +11,13 @@ var MenuLine = (function(){
 	
 	var init = function() {
 		var active = $('.nav-main a.active');
-		var width = active.width();
-		var left = active.position().left + parseInt(active.css('padding-left'));
+		if(active.length == 0) {
+			var width = 0;
+			var left = 0;
+		} else {
+			var width = active.width();
+			var left = active.position().left + parseInt(active.css('padding-left'));
+		}
 		set(left, width);
 	}
 
@@ -26,5 +31,45 @@ var MenuLine = (function(){
 		init();
 	});
 
-	init();
+	$(function(){
+		init();
+	});
+
+})();
+
+var List = (function(){
+
+	$('.us-page ol').each(function(){
+		var i = 1;
+		$(this).find('li').each(function(){
+			$(this).attr('data-number', i);
+			i++;
+		});
+	});
+
+})();
+
+var Parallax = (function(){
+
+	var init = function() {
+		if($('.parallax').length == 0) return;
+
+		var parPos = [];
+		
+		parPos[0] = $('.par-item').eq(0).offset().top;
+		parPos[1] = $('.par-item').eq(1).offset().top;
+
+		$(window).on('scroll', function(){
+			var scrollDown = $(window).scrollTop() + $(window).height();
+			console.log(parPos[0]);
+			if(parPos[0] < scrollDown) {
+				console.log(2);
+			}
+		});
+	}
+
+	$(function(){
+		init();
+	});
+
 })();
