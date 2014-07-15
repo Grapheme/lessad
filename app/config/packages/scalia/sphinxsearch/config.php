b0181df59ee7b2eae8605bbd6d944623c4868b9b
vkharseev <vladimir@realitygroup.ru>
2014-07-15 18:12:35 +0400

    Sphinx Search Test 3

--- app/config/packages/scalia/sphinxsearch/config.php
+++ app/config/packages/scalia/sphinxsearch/config.php
@@ -4,9 +4,9 @@ return array (
 	'host'    => '127.0.0.1',
 	'port'    => 3312,
 	'indexes' => array (
-		'channelsIndex' => FALSE,
-		'productsIndex' => FALSE,
-		'reviewsIndex' => FALSE,
-		'pagesIndex' => FALSE
+		'channelsIndex' => array('table'=>'channels','column'=>'id'),
+		'productsIndex' => array('table'=>'products','column'=>'id'),
+		'reviewsIndex' => array('table'=>'reviews','column'=>'id'),
+		'pagesIndex' => array('table'=>'i18n_pages','column'=>'id')
 	)
 );
