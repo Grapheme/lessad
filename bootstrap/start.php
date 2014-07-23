4a01c41ad84acff8ec79dc50cba016d2251b6ee1
KostyaD <thedamaxstudio@gmail.com>
2014-07-23 12:20:16 +0400

    environment

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -3,6 +3,7 @@
 $app = new Illuminate\Foundation\Application;
 $env = $app->detectEnvironment(array(
 	'local' => array('DNS'),
+	'dobriymac' => array('DobriyMac')
 ));
 $app->bindInstallPaths(require __DIR__.'/paths.php');
 $framework = $app['path.base'].'/vendor/laravel/framework/src';
