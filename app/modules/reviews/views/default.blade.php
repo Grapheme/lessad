f8b8d1d7a305596c6a6ddf139f948c3466a257c8
vkharseev <vkharseev@gmail.com>
2014-07-25 12:38:18 +0400

    Шаблон отзывов

--- app/modules/reviews/views/default.blade.php
+++ app/modules/reviews/views/default.blade.php
@@ -2,15 +2,17 @@
     <div class="wrapper">
         <div class="block-title">Отзывы</div>
         <ul class="feedb-messages">
-            @foreach(Reviews::orderBy('published_at','desc')->take(3)->with('photo')->get() as $review)
+            @foreach(Reviews::orderBy('published_at','desc')->take(3)->with('photo')->with('meta')->get() as $review)
             <li>
                 @if(!empty($review->photo))
                 <div class="feedb-photo" style="background-image: url(uploads/galleries/{{ $review->photo->name  }})"></div>
                 @endif
                 <div class="name">{{ $review->name }}</div>
                 <div class="post">{{ $review->position }}</div>
-                <div class="message">{{ $review->content }}</div>
-                <a href="#" class="review-link"></a>
+                <div class="message">{{ $review->preview }}</div>
+                @if (ReviewsController::$prefix_url !== FALSE):
+                <a href="{{ link::to(ReviewsController::$prefix_url.'/'.$review->meta->first()->seo_url) }}" class="review-link"></a>
+                @endif
             @endforeach
         </ul>
     </div>
