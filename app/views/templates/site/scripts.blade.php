1137f0634e117a7a04aeb3ae0c6c1f455e7f3397
KostyaD <thedamaxstudio@gmail.com>
2014-08-05 10:35:41 +0400

    jivosite

--- app/views/templates/site/scripts.blade.php
+++ app/views/templates/site/scripts.blade.php
@@ -15,3 +15,17 @@
 			loadScript("{{asset('js/vendor/jquery-form.min.js');}}");
 		}
 	</script>
+	
+	<!-- BEGIN JIVOSITE CODE {literal} -->
+
+	<script type='text/javascript'>
+
+		(function(){ var widget_id = 'DHYcVbBdhG';
+
+		var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//
+		code.jivosite.com/script/widget/'+widget_id
+		; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);})();
+
+	</script>
+
+	<!-- {/literal} END JIVOSITE CODE -->
