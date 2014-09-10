641b0366e1b86b1df264804d3e54dfacbeaf9700
Владимир <vkharseev@gmail.com>
2014-09-10 17:36:38 +0400

    Footer link to block

--- app/config/app.php
+++ app/config/app.php
@@ -13,7 +13,7 @@ return array(
 	'template' => 'site',	
 	'manifest' => storage_path('meta'),
 
-	'debug' => FALSE,
+	'debug' => true,
 	'url' => 'http://lessad.ru/',
 	'timezone' => 'UTC',
 	'locale' => 'ru',
